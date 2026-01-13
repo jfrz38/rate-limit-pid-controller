54e40c3ff66885bd1fb5af0875665cf4c6adb4d9
José F. Ruiz <jrz899@inlumine.ual.es>
2026-01-13 23:13:31 +0100

    [iss-12] export RejectedRequestException and parameters

---
+++ code/nodejs/src/domain/types/index.ts
@@ -0,0 +1,7 @@
+export * from './parameters';
+export * from './capacity';
+export * from './log';
+export * from './pid';
+export * from './statistics';
+export * from './threshold';
+export * from './timeout';
